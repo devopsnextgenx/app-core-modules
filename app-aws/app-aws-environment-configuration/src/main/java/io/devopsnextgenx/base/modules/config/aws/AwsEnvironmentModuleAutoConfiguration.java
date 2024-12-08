75f08659767ee749abdf08322b9713103fe5d6c9
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2024-12-08 18:08:37 -0500

    working localstack connection back

--- app-aws/app-aws-environment-configuration/src/main/java/io/devopsnextgenx/base/modules/config/aws/AwsEnvironmentModuleAutoConfiguration.java
+++ app-aws/app-aws-environment-configuration/src/main/java/io/devopsnextgenx/base/modules/config/aws/AwsEnvironmentModuleAutoConfiguration.java
@@ -10,9 +10,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import java.util.Optional;
 
 /**
  * SwaggerModuleAutoConfiguration:
