be7deb71bfd951ffc0f8aff88a4d8954b783d252
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2024-12-08 18:18:39 -0500

    working localstack connection back

--- app-security-jwt-selfsigned/src/test/java/io/devopsnextgenx/microservices/modules/security/jwt/SelfSignedJwtFilterTest.java
+++ app-security-jwt-selfsigned/src/test/java/io/devopsnextgenx/microservices/modules/security/jwt/SelfSignedJwtFilterTest.java
@@ -34,7 +34,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
-import static org.springframework.boot.test.util.TestPropertyValues.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -89,6 +88,7 @@ public class SelfSignedJwtFilterTest {
     public void givenFederatedUser_whenTokenHasFederatedClaim_accessDataShouldHaveFlagFederatedTrue() throws ServletException, IOException {
 
         String testSubjectName = "unknown-user@some-domain.com";
+        @SuppressWarnings("unchecked")
         String token = loginHelper.login(testSubjectName, Pair.of(JwtTokenAuthenticationFilter.CLAIM_USER_ARRAY, Collections.singletonMap(JwtTokenAuthenticationFilter.CLAIM_FEDERATED, true)));
         when(request.getHeader(eq(AuthHeaders.AUTHORIZATION_HEADER))).thenReturn(token);
 
@@ -102,6 +102,7 @@ public class SelfSignedJwtFilterTest {
     public void givenRegularUserWithUserArrayClaim_whenNoFederatedClaimExists_accessDataShouldHaveFlagFederatedFalse() throws ServletException, IOException {
 
         String testSubjectName = "unknown-user@some-domain.com";
+        @SuppressWarnings("unchecked")
         String token = loginHelper.login(testSubjectName, Pair.of(JwtTokenAuthenticationFilter.CLAIM_USER_ARRAY, Collections.singletonMap("SomethingElse", true)));
         when(request.getHeader(eq(AuthHeaders.AUTHORIZATION_HEADER))).thenReturn(token);
 
@@ -112,18 +113,19 @@ public class SelfSignedJwtFilterTest {
     }
 
 
-    @Test
-    public void when_validTokenHeader__controllersShouldReturnResponse() throws Exception {
+        @Test
+        public void when_validTokenHeader__controllersShouldReturnResponse() throws Exception {
 
-        String testSubjectName = "test@some-domain.com";
-        String token = loginHelper.login(testSubjectName, null);
-        when(request.getHeader(eq(AuthHeaders.AUTHORIZATION_HEADER))).thenReturn(token);
+            String testSubjectName = "test@some-domain.com";
+            @SuppressWarnings("unchecked")
+            String token = loginHelper.login(testSubjectName, (Pair<String, Object>[]) null);
+            when(request.getHeader(eq(AuthHeaders.AUTHORIZATION_HEADER))).thenReturn(token);
 
-        this.mockMvc.perform(get("/api/test").header(HttpHeaders.AUTHORIZATION, token)).andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString(TestController.TEST_REPLY_FROM_CONTROLLER)));
+            this.mockMvc.perform(get("/api/test").header(HttpHeaders.AUTHORIZATION, token)).andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(content().string(containsString(TestController.TEST_REPLY_FROM_CONTROLLER)));
 
-    }
+        }
 
     @Test
     public void when_invalidTokenHeader__controllersShouldFailUnAuthorized() throws Exception {
@@ -135,6 +137,7 @@ public class SelfSignedJwtFilterTest {
     @Test
     public void when_tokenExpired__controllersShouldFailTokenExpiredException() throws Exception {
 
+        @SuppressWarnings("unchecked")
         String token = loginHelper.login("unknown-user@some-domain.com", Pair.of(PublicClaims.EXPIRES_AT, new Date(10000)));
         System.out.println(token);
         when(request.getHeader(eq(AuthHeaders.AUTHORIZATION_HEADER))).thenReturn(token);
