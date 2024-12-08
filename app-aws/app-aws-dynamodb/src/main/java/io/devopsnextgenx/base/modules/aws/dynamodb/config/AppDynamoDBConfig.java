408f9effc56a0f8c39da7237e71190ced26ffe75
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2024-12-08 18:14:12 -0500

    working localstack connection back

--- app-aws/app-aws-dynamodb/src/main/java/io/devopsnextgenx/base/modules/aws/dynamodb/config/AppDynamoDBConfig.java
+++ app-aws/app-aws-dynamodb/src/main/java/io/devopsnextgenx/base/modules/aws/dynamodb/config/AppDynamoDBConfig.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.devopsnextgenx.base.modules.config.aws.AppAwsResourceProperties;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * AppDynamoDBConfig:
@@ -14,6 +15,7 @@ import lombok.Data;
  * @since 12/15/2019
  */
 @Data
+@EqualsAndHashCode(callSuper = false)
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class AppDynamoDBConfig extends AppAwsResourceProperties {
     private DynamoDBMapperConfig.SaveBehavior saveBehavior = DynamoDBMapperConfig.SaveBehavior.PUT;
--- app-aws/app-aws-dynamodb/src/main/java/io/devopsnextgenx/base/modules/aws/dynamodb/config/YamlPropertyLoaderFactory.java
+++ app-aws/app-aws-dynamodb/src/main/java/io/devopsnextgenx/base/modules/aws/dynamodb/config/YamlPropertyLoaderFactory.java
@@ -3,17 +3,28 @@ package io.devopsnextgenx.base.modules.aws.dynamodb.config;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PropertySourceFactory;
+
+import io.micrometer.common.lang.NonNull;
+import io.micrometer.common.lang.Nullable;
+
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import java.io.IOException;
+import java.util.Properties;
+
 import org.springframework.core.env.PropertiesPropertySource;
 
 
 public class YamlPropertyLoaderFactory implements PropertySourceFactory {
 
     @Override
-    public PropertySource<?> createPropertySource(String name, EncodedResource encodedResource) throws IOException {
+    @SuppressWarnings("null")
+    public @NonNull PropertySource<?> createPropertySource(@Nullable String name, @NonNull EncodedResource encodedResource) throws IOException {
         YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
         factory.setResources(encodedResource.getResource());
-        return new PropertiesPropertySource(encodedResource.getResource().getFilename(), factory.getObject());
+        Properties properties = factory.getObject();
+        if (properties != null) {
+            return new PropertiesPropertySource(encodedResource.getResource().getFilename(), properties);
+        }
+        throw new IOException("Failed to load YAML properties from resource");
     }
 }
