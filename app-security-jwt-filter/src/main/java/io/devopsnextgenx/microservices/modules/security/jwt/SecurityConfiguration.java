8681e5019386d69b4a6c9728e06f6986b771d73c
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2024-12-08 21:36:33 -0500

    pending security implementation from scratch

--- app-security-jwt-filter/src/main/java/io/devopsnextgenx/microservices/modules/security/jwt/SecurityConfiguration.java
+++ app-security-jwt-filter/src/main/java/io/devopsnextgenx/microservices/modules/security/jwt/SecurityConfiguration.java
@@ -36,124 +36,128 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
-@EnableWebSecurity
+// @EnableWebSecurity
 @EnableConfigurationProperties
-@PropertySources({ @PropertySource(name = "oauthApplications", value = {
-        "${APPCONFIGFILE}" }, ignoreResourceNotFound = true, factory = YamlPropertyLoaderFactory.class) })
+@PropertySources({
+    @PropertySource(name = "oauthApplications", 
+        value = { "${APPCONFIGFILE}" },
+        ignoreResourceNotFound = true,
+        factory = YamlPropertyLoaderFactory.class) 
+})
 public class SecurityConfiguration {
 
-    @Bean
-    @ConfigurationProperties("app.oauth")
-    public OAuthApplicationsConfig oauthApplicationsConfig() {
-        return new OAuthApplicationsConfig();
-    }
+    // @Bean
+    // @ConfigurationProperties("app.oauth")
+    // public OAuthApplicationsConfig oauthApplicationsConfig() {
+    //     return new OAuthApplicationsConfig();
+    // }
 
-    @Bean
-    @ConfigurationProperties(prefix = "app.auth.app.userservice")
-    public AppConfig appConfig() {
-        return new AppConfig();
-    }
+    // @Bean
+    // @ConfigurationProperties(prefix = "app.auth.app.userservice")
+    // public AppConfig appConfig() {
+    //     return new AppConfig();
+    // }
 
-    @Bean
-    @ConfigurationProperties(prefix = "app.auth.swagger")
-    public SwaggerUISecurityConfig swaggerUISecurityConfig() {
-        return new SwaggerUISecurityConfig();
-    }
+    // @Bean
+    // @ConfigurationProperties(prefix = "app.auth.swagger")
+    // public SwaggerUISecurityConfig swaggerUISecurityConfig() {
+    //     return new SwaggerUISecurityConfig();
+    // }
 
-    @Bean
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    public OAuthLoginHelper oAuthLoginHelper(OAuthApplicationsConfig oAuthApplicationsConfig) {
-        return new OAuthLoginHelper(oAuthApplicationsConfig);
-    }
+    // @Bean
+    // @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    // public OAuthLoginHelper oAuthLoginHelper(OAuthApplicationsConfig oAuthApplicationsConfig) {
+    //     return new OAuthLoginHelper(oAuthApplicationsConfig);
+    // }
 
-    @Bean
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    public JWTVerifierCache audienceVerifierCache(OAuthLoginHelper oAuthLoginHelper, RestTemplate restTemplate,
-            AppConfig appConfig, @Value("${app.services.userServiceHost}") String userServiceHost) {
-        return new JWTVerifierCache(oAuthLoginHelper, restTemplate, new JwtVerifierHelper(), appConfig,
-                userServiceHost);
-    }
+    // @Bean
+    // @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    // public JWTVerifierCache audienceVerifierCache(OAuthLoginHelper oAuthLoginHelper, RestTemplate restTemplate,
+    //         AppConfig appConfig, @Value("${app.services.userServiceHost}") String userServiceHost) {
+    //     return new JWTVerifierCache(oAuthLoginHelper, restTemplate, new JwtVerifierHelper(), appConfig,
+    //             userServiceHost);
+    // }
 
-    @Bean
-    @ConditionalOnMissingBean(TokenValidator.class)
-    public TokenValidator tokenValidator(OAuthApplicationsConfig oAuthApplicationsConfig,
-            JWTVerifierCache jwtVerifierCache) {
-        Assert.notNull(oAuthApplicationsConfig,
-                String.format("Missing required configurations for OAuth security applications"));
-        Assert.notEmpty(oAuthApplicationsConfig.getApplications(),
-                "Configurations for OAuth security applications is empty");
-        return new ProductionTokenValidator(oAuthApplicationsConfig, jwtVerifierCache, new JwtVerifierHelper());
-    }
+    // @Bean
+    // @ConditionalOnMissingBean(TokenValidator.class)
+    // public TokenValidator tokenValidator(OAuthApplicationsConfig oAuthApplicationsConfig,
+    //         JWTVerifierCache jwtVerifierCache) {
+    //     Assert.notNull(oAuthApplicationsConfig,
+    //             String.format("Missing required configurations for OAuth security applications"));
+    //     Assert.notEmpty(oAuthApplicationsConfig.getApplications(),
+    //             "Configurations for OAuth security applications is empty");
+    //     return new ProductionTokenValidator(oAuthApplicationsConfig, jwtVerifierCache, new JwtVerifierHelper());
+    // }
 
-    @Bean
-    public JwtTokenAuthenticationFilter jwtTokenAuthenticationFilter(TokenValidator tokenValidator) {
-        log.info("security-jwt-filter: Injected TokenValidator type: '{}' ", tokenValidator.getClass().getSimpleName());
-        return new JwtTokenAuthenticationFilter(tokenValidator);
-    }
+    // @Bean
+    // public JwtTokenAuthenticationFilter jwtTokenAuthenticationFilter(TokenValidator tokenValidator) {
+    //     log.info("security-jwt-filter: Injected TokenValidator type: '{}' ", tokenValidator.getClass().getSimpleName());
+    //     return new JwtTokenAuthenticationFilter(tokenValidator);
+    // }
 
-    @SuppressWarnings("deprecation")
-    @Bean
-    public UserDetailsService userDetailsService(SwaggerUISecurityConfig appSwaggerUISecurityConfig) {
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(users.username(
-                appSwaggerUISecurityConfig.getSwaggerUser())
-                .password(appSwaggerUISecurityConfig.getSwaggerUserPassword())
-                .roles("USER").build());
-        manager.createUser(users.username(
-                appSwaggerUISecurityConfig.getSwaggerAdmin())
-                .password(appSwaggerUISecurityConfig.getSwaggerAdminPassword())
-                .roles("USER", "ADMIN").build());
-        return manager;
-    }
+    // @SuppressWarnings("deprecation")
+    // @Bean
+    // public UserDetailsService userDetailsService(SwaggerUISecurityConfig appSwaggerUISecurityConfig) {
+    //     User.UserBuilder users = User.withDefaultPasswordEncoder();
+    //     InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+    //     manager.createUser(users.username(
+    //             appSwaggerUISecurityConfig.getSwaggerUser())
+    //             .password(appSwaggerUISecurityConfig.getSwaggerUserPassword())
+    //             .roles("USER").build());
+    //     manager.createUser(users.username(
+    //             appSwaggerUISecurityConfig.getSwaggerAdmin())
+    //             .password(appSwaggerUISecurityConfig.getSwaggerAdminPassword())
+    //             .roles("USER", "ADMIN").build());
+    //     return manager;
+    // }
 
-    @Configuration
-    @Order(1)
-    public static class SwaggerActuatorBasicSecurityConfig {
+    // @Configuration
+    // @Order(1)
+    // public static class SwaggerActuatorBasicSecurityConfig {
 
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            http
-                    .authorizeHttpRequests((authz) -> authz
-                            .anyRequest().authenticated())
-                    .httpBasic(Customizer.withDefaults());
-            return http.build();
-        }
-    }
+    //     @Bean
+    //     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    //         http
+    //                 .authorizeHttpRequests((authz) -> authz
+    //                         .anyRequest().authenticated())
+    //                 .httpBasic(Customizer.withDefaults());
+    //         return http.build();
+    //     }
+    // }
 
-    @Configuration
-    @Order(2)
-    public class ApiJwtSecurityConfig {
+    // @Configuration
+    // @Order(2)
+    // public class ApiJwtSecurityConfig {
 
-        @Autowired
-        @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-        private JwtTokenAuthenticationFilter jwtTokenAuthenticationFilter;
+    //     @Autowired
+    //     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    //     private JwtTokenAuthenticationFilter jwtTokenAuthenticationFilter;
 
-        @Bean
-        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-            http.sessionManagement(management -> management
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                    .csrf(csrf -> csrf.disable())
-                    // handle an authorized attempts
-                    .exceptionHandling(handling -> handling
-                            .authenticationEntryPoint(
-                                    (req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)))
-                    // Add a filter to validate the tokens with every request
-                    .addFilterAfter(jwtTokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                    // authorization requests config
-                    .authorizeHttpRequests(request -> request.requestMatchers("/api/**")
-                            .authenticated());
-            return http.build();
-        }
-    }
+    //     @Bean
+    //     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    //         http.sessionManagement(management -> management
+    //                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    //                 .csrf(csrf -> csrf.disable())
+    //                 // handle an authorized attempts
+    //                 .exceptionHandling(handling -> handling
+    //                         .authenticationEntryPoint(
+    //                                 (req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)))
+    //                 // Add a filter to validate the tokens with every request
+    //                 .addFilterAfter(jwtTokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+    //                 // authorization requests config
+    //                 .authorizeHttpRequests(request -> request.requestMatchers("/api/**")
+    //                         .authenticated());
+    //         return http.build();
+    //     }
+    // }
 
-    @Configuration
-    @Order(3)
-    public class HystrixNoAuthSecurityConfig {
-        @Bean
-        public WebSecurityCustomizer webSecurityCustomizer() {
-            return (web) -> web.ignoring().requestMatchers("/actuator/hystrix.stream", "/turbine.stream",
-                    "/actuator/health", "/HealthCheck", "/health", "/metrics");
-        }
-    }
+    // @Configuration
+    // @Order(3)
+    // public class HystrixNoAuthSecurityConfig {
+    //     @Bean
+    //     public WebSecurityCustomizer webSecurityCustomizer() {
+    //         return (web) -> web.ignoring().requestMatchers("/actuator/hystrix.stream", "/turbine.stream",
+    //                 "/actuator/health", "/HealthCheck", "/health", "/metrics", "/actuator");
+    //     }
+    // }
 }
