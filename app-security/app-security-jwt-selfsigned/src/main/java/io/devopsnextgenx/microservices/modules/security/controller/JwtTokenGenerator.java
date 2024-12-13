93db3a2cb0dad2c707d68f7c7c7a9da4b2fc1c61
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2024-12-12 20:21:04 -0500

    working jwt/basicAuth and prometheus

--- app-security/app-security-jwt-selfsigned/src/main/java/io/devopsnextgenx/microservices/modules/security/controller/JwtTokenGenerator.java
+++ app-security/app-security-jwt-selfsigned/src/main/java/io/devopsnextgenx/microservices/modules/security/controller/JwtTokenGenerator.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
