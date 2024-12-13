583d88dc02c6416692632b1bc0bd85cf7988d0ed
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2024-12-12 20:21:11 -0500

    working jwt/basicAuth and prometheus

--- app-micro-capabilities/app-micro-swagger/src/main/java/io/devopsnextgenx/microservices/modules/swagger/SwaggerSecurityConfiguration.java
+++ app-micro-capabilities/app-micro-swagger/src/main/java/io/devopsnextgenx/microservices/modules/swagger/SwaggerSecurityConfiguration.java
@@ -1,7 +1,5 @@
 package io.devopsnextgenx.microservices.modules.swagger;
 
-import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
