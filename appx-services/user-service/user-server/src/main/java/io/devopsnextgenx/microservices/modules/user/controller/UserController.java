5ec08f1ada0909278f66f092fec70b4f9bbfc8bd
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2024-12-15 18:26:06 -0500

    pending https config connection

--- appx-services/user-service/user-server/src/main/java/io/devopsnextgenx/microservices/modules/user/controller/UserController.java
+++ appx-services/user-service/user-server/src/main/java/io/devopsnextgenx/microservices/modules/user/controller/UserController.java
@@ -1,39 +1,39 @@
-package io.devopsnextgenx.microservices.controller;
+// package io.devopsnextgenx.microservices.controller;
 
-import java.util.UUID;
+// import java.util.UUID;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.MediaType;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RestController;
 
-import io.devopsnextgenx.microservices.modules.api.UserApi;
-import io.devopsnextgenx.microservices.modules.model.User;
-import lombok.extern.slf4j.Slf4j;
+// import io.devopsnextgenx.microservices.modules.api.UserApi;
+// import io.devopsnextgenx.microservices.modules.model.User;
+// import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
-@RestController
-@RequestMapping("/api")
-public class UserController implements UserApi {
+// @Slf4j
+// @RestController
+// @RequestMapping("/api")
+// public class UserController implements UserApi {
 
-    @RequestMapping(value = "/echo", method = {RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE })
-    public ResponseEntity<String> echo() {
-        log.info("echo endpoint invoked");
-        return new ResponseEntity<>("{'message': 'User'}", HttpStatus.OK);
-    }
+//     @RequestMapping(value = "/echo", method = {RequestMethod.GET}, produces = {MediaType.APPLICATION_JSON_VALUE })
+//     public ResponseEntity<String> echo() {
+//         log.info("echo endpoint invoked");
+//         return new ResponseEntity<>("{'message': 'User'}", HttpStatus.OK);
+//     }
 
-    @Override
-    public ResponseEntity<User> getUserById(UUID id) {
-        User user = new User().email("test").firstName("amit").lastName("kshirsagar").id(id);
-        return ResponseEntity.ok(user);
-    }
+//     @Override
+//     public ResponseEntity<User> getUserById(UUID id) {
+//         User user = new User().email("test").firstName("amit").lastName("kshirsagar").id(id);
+//         return ResponseEntity.ok(user);
+//     }
 
-    @Override
-    public ResponseEntity<Void> deleteUserById(UUID id) {
-        return ResponseEntity.status(201).body(null);
-    }
+//     @Override
+//     public ResponseEntity<Void> deleteUserById(UUID id) {
+//         return ResponseEntity.status(201).body(null);
+//     }
 
     
-}
+// }
