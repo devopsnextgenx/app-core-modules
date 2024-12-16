f1e71d9dea511f67a0fbe9fa174fc701375e6790
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2024-12-15 20:34:36 -0500

    pending logging issue with service module

--- appx-services/user-service/user-server/ReadMe.md
+++ appx-services/user-service/user-server/ReadMe.md
@@ -14,6 +14,10 @@ Can be configured to use any port on container by defining the VPORT variable.
 
 
 ### Echo Url:
-https://user-service.k8s.localtest.me:8080/api/echo
+Sample endpoint for echo: https://user-service.k8s.localtest.me:8080/api/echo
 
 Get User by Id: https://user-service.appx.localtest.me:8080/api/user/123e4567-e89b-42d3-a456-556642440000
+
+
+### Swagger Url:
+Swagger endpoint : https://user-service.appx.localtest.me:8080/swagger-ui/index.html
--- appx-services/user-service/user-server/src/main/java/io/devopsnextgenx/microservices/modules/user/UserApplication.java
+++ appx-services/user-service/user-server/src/main/java/io/devopsnextgenx/microservices/modules/user/UserApplication.java
@@ -2,10 +2,15 @@ package io.devopsnextgenx.microservices.modules.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-// @EnableDiscoveryClient
+@EnableDiscoveryClient
+// @SpringBootApplication(exclude = {
+// 	org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+// })
+@ComponentScan(basePackages = "io.devopsnextgenx")
 public class UserApplication {
 
 	public static void main(String[] args) {
