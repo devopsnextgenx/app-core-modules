acdd42bdcdcfb793978995d56d75bf0d84b14806
Amit Kshirsagar <amit.kshirsagar.13@gmail.com>
2024-12-18 15:21:22 -0500

    updated refactors

--- appx-services/app-user-auth-service/user-auth-service/src/main/java/io/devopsnextgenx/microservices/modules/UserAuthApplication.java
+++ appx-services/app-user-auth-service/user-auth-service/src/main/java/io/devopsnextgenx/microservices/modules/UserAuthApplication.java
@@ -23,6 +23,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -34,6 +35,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
+@ComponentScan("io.devopsnextgenx.microservices.modules")
 @EnableJpaRepositories("io.devopsnextgenx.microservices.modules")
 public class UserAuthApplication {
     public static void main(String[] args) {
