f87645dfb6fc08a288cb3b3e2741d0b8fd33b95d
Amit Kshirsagar <amit.kshirsagar@siemens.com>
2024-12-19 22:15:49 -0500

    modelmapper typeMap used for skipping field in Dto

--- appx-services/app-user-auth-service/user-auth-service/src/main/java/io/devopsnextgenx/microservices/modules/models/providers/UserCloner.java
+++ appx-services/app-user-auth-service/user-auth-service/src/main/java/io/devopsnextgenx/microservices/modules/models/providers/UserCloner.java
@@ -1,11 +1,24 @@
 package io.devopsnextgenx.microservices.modules.models.providers;
 
+import org.modelmapper.PropertyMap;
+import org.modelmapper.TypeMap;
+
 import io.devopsnextgenx.microservices.modules.models.User;
 import io.devopsnextgenx.microservices.modules.userauth.user.dto.UserDto;
 
 public class UserCloner extends AbstractCloner<User, UserDto> {
     @Override
     public void updateTypeMap() {
+        TypeMap<User, UserDto> propMap = getModelMapper().createTypeMap(User.class, UserDto.class);
+        propMap.addMappings(mapping -> {
+            mapping.skip(UserDto::setPassword);
+        });
+        // PropertyMap<User, UserDto> skipModifiedFieldsMap = new PropertyMap<User, UserDto>() {
+        //     protected void configure() {
+        //         skip().setPassword(null);;
+        //     }
+        // };
+        // propMap.addMappings(skipModifiedFieldsMap);
     }
 
     @Override
